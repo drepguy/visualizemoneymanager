bc74ec709b98abe4534f9a64cff9f219d33f1448
Julian K <20781740+drepguy@users.noreply.github.com>
2024-12-16 20:44:42 +0100

    cleanup and commentary

--- SankeyStringCreater.py
+++ SankeyStringCreater.py
@@ -1,5 +1,7 @@
 # this files containing functions to create the sankey string
 
+import logging
+
 # create a function to create the sankey string
 def generate_sankey_string(category_sums, subcategory_sums):
 
@@ -14,30 +16,34 @@ def generate_sankey_string(category_sums, subcategory_sums):
     # create a list to store the sankey string
     sankeystring = []
 
-    #
+
+    # iterate through all categories_sums
     for category, sums in category_sums.items():
         if sums['income'] > 0:
             sankeystring.append(f"{category} [{round(sums['income'], 2)}] Budget")
         if sums['outcome'] > 0:
             sankeystring.append(f"Budget [{round(sums['outcome'], 2)}] {category}")
 
+    # iterate through all subcategories_sums
     for subcategory, sums in subcategory_sums.items():
         if sums['income'] > 0:
             sankeystring.append(f"{subcategory[0]}:{subcategory[1]} [{round(sums['income'], 2)}] {sums['category']}")
         if sums['outcome'] > 0:
             sankeystring.append(f"{sums['category']} [{round(sums['outcome'], 2)}] {subcategory[0]}:{subcategory[1]}")
 
+    # add all main categories sums together to get the total income and outcome
     sum_income = sum([sums['income'] for sums in category_sums.values()])
     sum_outcome = sum([sums['outcome'] for sums in category_sums.values()])
 
-    print("Budget: ", max(sum_income, sum_outcome))
-
+    # we need to add a Übertrag to keep sankey diagram balanced (inflow should be equal to outflow)
     if sum_income > sum_outcome:
         sankeystring.append(f"Budget [{round(sum_income-sum_outcome,2)}] Übertrag")
     else:
         sankeystring.append(f"Übertrag [{round(sum_outcome-sum_income,2)}] Budget")
 
     # handling for empty subcategories
+    # if subcategory is empty, we need to add a Weiteres subcategory to keep sankey diagram balanced, and put the difference there
+    # without it in case of outcome, we would have more inflow than outflow
     for category, category_sum in category_sums.items():
         subcategory_income_total = sum(
             sums['income'] for (subcategory, cat), sums in subcategory_sums.items() if cat == category and subcategory[0] != '')
@@ -49,19 +55,23 @@ def generate_sankey_string(category_sums, subcategory_sums):
 
         if outcome_diff >= 0.01:
             if any(cat == category for (_, cat) in subcategory_sums):
-                print(f"Category '{category}' outcome differs by {outcome_diff:.2f} euros")
+                # log the difference
+                logging.info(f"Category '{category}' outcome differs by {outcome_diff:.2f} euros")
+                # add the difference to the sankey string (basically create a new subcategory for it called Weiteres)
                 sankeystring.append(f"{category} [{outcome_diff:.2f}] Weiteres:{category}")
 
         if income_diff >= 0.01:
             if any(cat == category for (_, cat) in subcategory_sums):
-                print(f"Category '{category}' outcome differs by {income_diff:.2f} euros")
+                # log the difference
+                logging.info(f"Category '{category}' income differs by {income_diff:.2f} euros")
+                # add the difference to the sankey string (basically create a new subcategory for it called Weiteres)
                 sankeystring.append(f"Weiteres:{category} [{income_diff:.2f}] {category}")
 
-    return "\n".join(sankeystring)
+    return '\n'.join(sankeystring)
 
 # noinspection SpellCheckingInspection
 def append_settings_to_string(sankeystring):
-    return sankeystring + '\r\n' +  """
+    return sankeystring + '\r\n' +  '''
 // === Settings ===
 size w 900
   h 1400
@@ -114,4 +124,4 @@ themeoffset a 9
   d 0
 meta mentionsankeymatic Y
   listimbalances Y
-"""
+'''
--- main.py
+++ main.py
@@ -14,20 +14,34 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
 
+    # check if the file exists
     if not os.path.exists(FILE_PATH):
+        # if the file does not exist, log an error and exit the program
         logging.error("The file does not exist.")
+        # exit the program with an error code
         sys.exit(1)
 
+    # parse the MoneyManager export file
     entries = parse_moneymanager_export(FILE_PATH)
-    print_entries(entries)
 
+    # log all entries with new line in between them
+    logging.info("\n".join([str(entry) for entry in entries]))
+
+    # calculate the sums for the categories and subcategories
     category_sums, subcategory_sums = calculate_sums(entries)
 
+    # generate the sankey raw string
     sankey_string = generate_sankey_string(category_sums, subcategory_sums)
+
+    # append the settings to the sankey string
     sankey_string = append_settings_to_string(sankey_string)
+
+    # print the sankey string
     logging.info(sankey_string)
 
+    # copy the sankey string to the clipboard
     pyperclip.copy(sankey_string)
+
     logging.info("Sankey string copied to clipboard")
 
 if __name__ == '__main__':
